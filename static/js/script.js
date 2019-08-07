babc86b5b76c303f3ead2cd37890aeccb0662929
Ubuntu <ubuntu@ip-172-31-82-5.ec2.internal>
2019-08-07 14:06:05 +0000

    add line graph

--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,87 @@
 queue()
     .defer(d3.json, 'data/epa-sea-level.csv')
     .await(countdown);
+
+//countdown
+
+function countdown() {
+    var startDate = new Date();
+    var endDate = new Date('March 28, 2030 00:00:00'); //11 years from UN report publication date
+
+    var startTime = startDate.getTime();
+    var endTime = endDate.getTime();
+
+    var timeRemaining = endTime - startTime;
+
+    var seconds = Math.floor(timeRemaining / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+    var months = Math.floor(days / 30.44); //average number of days in a month
+    var years = Math.floor(months / 12);
+
+    months = months % 12;
+    days = Math.floor(days % 30.44);
+    hours = hours % 24;
+    minutes %= 60;
+    seconds %= 60;
+
+    months = (months < 10) ? '0' + months : months;
+    days = (days < 10) ? '0' + days : days;
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    document.getElementById('years').innerHTML = years;
+    document.getElementById('months').innerHTML = months;
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    setTimeout(countdown, 1000);
+}
+
+//sea level rise line graph
+
+d3.csv("data/epa-sea-level.csv").get(function(error, data) {
+    if (error) throw error;
     
-    function countdown() {
-        var startDate = new Date();
-        var endDate = new Date('March 28, 2030 00:00:00'); //11 years from UN report publication date
+    var height = 500;
+    var width = 500;
 
-        var startTime = startDate.getTime();
-        var endTime = endDate.getTime();
+    var maxRise = d3.max(data, function(d) { return d.CSIRO_Adjusted_Sea_Level; });
 
-        var timeRemaining = endTime - startTime;
+    var maxDate = d3.max(data, function(d) { return d.Year; });
+    var minDate = d3.min(data, function(d) { return d.Year; });
 
-        var seconds = Math.floor(timeRemaining / 1000);
-        var minutes = Math.floor(seconds / 60);
-        var hours = Math.floor(minutes / 60);
-        var days = Math.floor(hours / 24);
-        var months = Math.floor(days / 30.44); //average number of days in a month
-        var years = Math.floor(months / 12);
+    var n = 135; //number of data points
 
-        months = months % 12;
-        days = Math.floor(days % 30.44);
-        hours = hours % 24;
-        minutes %= 60;
-        seconds %= 60;
+    var y = d3.scaleLinear()
+        .domain([0, maxRise])
+        .range([height, 0]);
 
-        months = (months < 10) ? '0' + months : months;
-        days = (days < 10) ? '0' + days : days;
-        hours = (hours < 10) ? '0' + hours : hours;
-        minutes = (minutes < 10) ? '0' + minutes : minutes;
-        seconds = (seconds < 10) ? '0' + seconds : seconds;
+    var x = d3.scaleTime()
+        .domain([minDate, maxDate])
+        .range([0, width]);
 
-        document.getElementById('years').innerHTML = years;
-        document.getElementById('months').innerHTML = months;
-        document.getElementById('days').innerHTML = days;
-        document.getElementById('hours').innerHTML = hours;
-        document.getElementById('minutes').innerHTML = minutes;
-        document.getElementById('seconds').innerHTML = seconds;
+    var yAxis = d3.axisLeft(y);
+    var xAxis = d3.axisBottom(x);
 
-        setTimeout(countdown, 1000);
+    var svg = d3.select('#sea_level_rise')
+        .append('svg')
+        .attr('height', '100%')
+        .attr('width', '100%');
 
-    }
+    var margin = { left: 50, right: 50, top: 40, bottom: 0 };
+
+    var chartGroup = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var line = d3.line()
+        .x(function(d) { return x(d.Year); })
+        .y(function(d) { return y(d.CSIRO_Adjusted_Sea_Level); });
+
+    chartGroup.append("path").attr("d", line(data));
+});
+
