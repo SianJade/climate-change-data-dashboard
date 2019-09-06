ddf0adb5cf19bc36be8ee5653fdef73c8ec65f1c
Ubuntu <ubuntu@ip-172-31-82-5.ec2.internal>
2019-09-06 22:41:35 +0000

    Add smooth scroll to jumbotron and page divider buttons

--- static/js/script.js
+++ static/js/script.js
@@ -429,3 +429,13 @@ function buildFootprintReductionGraph(footprint_ndx) {
         .dimension(changeDim)
         .group(emissionsReduction);
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
